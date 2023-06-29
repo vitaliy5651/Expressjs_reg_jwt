ee4ba69bfafe64bf7810249d17c816f518a59530
Vitaliy <vitaliy.kaliskarov@mail.ru>
2023-06-29 11:43:11 +0300

    edit auth

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,10 @@
 import jsonwebtoken from "jsonwebtoken";
 
 const auth = async (req, res, next) =>{
-    const [strategy, token] = req.header('Authorization').split(" ")
-    const result = jsonwebtoken.verify(token, 'secret')
-    req.login = result.login
     try{
+        const [strategy, token] = req.header('Authorization').split(" ")
+        const result = jsonwebtoken.verify(token, 'secret')
+        req.login = result.login
         next()
     }catch{
         res.status(400).send('Неверный токен')
